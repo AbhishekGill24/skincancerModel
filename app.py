6bb950a6738fb3183930248482f82b7a625f3a0e
AbhishekGill24 <110418373+AbhishekGill24@users.noreply.github.com>
2023-08-31 15:25:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,93 +1,84 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-from flask import Flask, render_template, request, redirect, flash
-from werkzeug.utils import secure_filename
-# from main import getPrediction
 import os
-from pyngrok import ngrok
-import uvicorn
-from flask_ngrok import run_with_ngrok
-
 import numpy as np
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
 from PIL import Image
-from sklearn.preprocessing import LabelEncoder
 from tensorflow.keras.models import load_model
+from sklearn.preprocessing import LabelEncoder
 
-
-# In[2]:
-
-
-def getPrediction(filename):
-    
-    classes = ['Actinic keratoses', 'Basal cell carcinoma', 
-               'Benign keratosis-like lesions', 'Dermatofibroma', 'Melanoma', 
-               'Melanocytic nevi', 'Vascular lesions']
-    le = LabelEncoder()
-    le.fit(classes)
-    le.inverse_transform([2])
-    
-    
-    #Load model
-    my_model=load_model("models/model.h5")
-    
-    SIZE = 32 #Resize to same size as training images
-    img_path = 'static/images/'+filename
-    img = np.asarray(Image.open(img_path).resize((SIZE,SIZE)))
-    
-    img = img/255.      #Scale pixel values
-    
-    img = np.expand_dims(img, axis=0)  #Get it tready as input to the network       
-    
-    pred = my_model.predict(img) #Predict                    
-    
-    #Convert prediction to class name
-    pred_class = le.inverse_transform([np.argmax(pred)])[0]
-    print("Diagnosis is:", pred_class)
-    return pred_class
-
-
-# In[4]:
-
-
-UPLOAD_FOLDER = 'static/images/'
+# Create a Flask web application
 app = Flask(__name__, static_folder="static")
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+# Set a secret key for the application (used for session management)
+app.secret_key = "secret"
+
+# Define the folder where uploaded images will be stored
+app.config['UPLOAD_FOLDER'] = 'static/images/'
+
+# Function to classify skin conditions using a trained model
+def classify_skin_condition(filename):
+    labels = ['Actinic keratoses', 'Basal cell carcinoma',
+              'Benign keratosis-like lesions', 'Dermatofibroma',
+              'Melanoma', 'Melanocytic nevi', 'Vascular lesions'] 
+    
+    # Create a label encoder and fit it to the labels
+    label_encoder = LabelEncoder()
+    label_encoder.fit(labels)
+
+    # Load a pre-trained Keras model for classification
+    model = load_model("models/model.h5")
+    
+    image_size = 32
+    image_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+    
+    # Open and resize the image using PIL (Python Imaging Library)
+    image = np.asarray(Image.open(image_path).resize((image_size, image_size)))
+    
+    # Normalize pixel values to the range [0, 1]
+    image = image / 255.0
+    
+    # Add a batch dimension to the image
+    image = np.expand_dims(image, axis=0)
+    
+    # Perform predictions using the loaded model
+    predictions = model.predict(image)
+    
+    # Inverse transform the predicted label index to get the actual class label
+    predicted_label = label_encoder.inverse_transform([np.argmax(predictions)])[0]
+    
+    return predicted_label
+
+# Route for the home page
 @app.route('/')
-def index():
+def home():
     return render_template('index.html')
 
+# Route for handling file uploads and skin condition classification
 @app.route('/', methods=['POST'])
-def submit_file():
+def upload_file():
     if request.method == 'POST':
         if 'file' not in request.files:
             flash('No file part')
             return redirect(request.url)
+        
         file = request.files['file']
         if file.filename == '':
             flash('No file selected for uploading')
             return redirect(request.url)
+        
         if file:
-            filename = secure_filename(file.filename)  
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
-            #getPrediction(filename)
-            label = getPrediction(filename)
-            flash(label)
-            full_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-            flash(full_filename)
+            filename = secure_filename(file.filename)
+            file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+            file.save(file_path)
+            
+            # Classify the uploaded image and get the diagnosis
+            diagnosis = classify_skin_condition(filename)
+            
+            # Flash the diagnosis and file path to display in the web interface
+            flash(diagnosis)
+            flash(file_path)
 
-
+# Run the application if this script is executed directly
 if __name__ == '__main__':
-   app.run(port=5002)
-
-
-# In[ ]:
-
-
-
-
+    # Start the Flask application on port 5002
+    app.run(port=5002)
